6f8d4230106b7a70f2144967d7d3e914b49742e0
sushi.at <sushi@opensky.to>
2021-12-03 12:08:00 +0000

    Empty project templates and git settings

---
+++ OpenSky.FlightLogXML.Tests/AirportPackageTests.cs
@@ -0,0 +1,28 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AirportPackageTests.cs" company="OpenSky">
+// OpenSky project 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.AirportsJSON.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Airports package tests.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 03/12/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    [TestClass]
+    public class AirportPackageTests
+    {
+        [TestMethod]
+        public void TestGeneratePackage()
+        {
+
+        }
+    }
+}
