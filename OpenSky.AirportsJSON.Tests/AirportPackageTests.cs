006952d9cd5bb04cbf9e31f51eecb2fbb7cc57f0
sushi.at <sushi@opensky.to>
2021-12-03 13:49:49 +0000

    Added existing code and test
    Simplified names and create simple create and parse package methods

--- OpenSky.AirportsJSON.Tests/AirportPackageTests.cs
+++ OpenSky.AirportsJSON.Tests/AirportPackageTests.cs
@@ -6,7 +6,11 @@
 
 namespace OpenSky.AirportsJSON.Tests
 {
+    using System.Diagnostics;
+
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Newtonsoft.Json;
 
     /// -------------------------------------------------------------------------------------------------
     /// <summary>
@@ -19,10 +23,116 @@ namespace OpenSky.AirportsJSON.Tests
     [TestClass]
     public class AirportPackageTests
     {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Test package generation, hash comparison and package parsing.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/12/2021.
+        /// </remarks>
+        /// -------------------------------------------------------------------------------------------------
         [TestMethod]
-        public void TestGeneratePackage()
+        public void TestGenerateAndParsePackage()
         {
+            var package = new AirportPackage();
+            Debug.WriteLine("Empty package:");
+            var json = package.CreatePackage(false);
+            Debug.WriteLine(json);
+            Debug.WriteLine(package.Hash);
+            var emptyHash = package.Hash;
 
+            // Make sure multiple runs don't change the hash
+            package.CreatePackage();
+            Assert.AreEqual(emptyHash, package.Hash);
+
+            var parsedPackage = AirportPackage.FromPackageJson(json, false);
+            Assert.AreEqual(parsedPackage.Hash, package.Hash);
+            Assert.AreEqual(parsedPackage.CreatePackage(false), package.CreatePackage(false));
+            Assert.AreEqual(parsedPackage.CreatePackage(), package.CreatePackage());
+
+            var loww = new Airport
+            {
+                ICAO = "LOWW",
+                HasAvGas = true,
+                HasJetFuel = true,
+                IsClosed = false,
+                IsMilitary = false,
+                Latitude = 48.11027908325195,
+                Longitude = 16.569721221923828,
+                MSFS = true,
+                Name = "Schwechat",
+                Size = 5,
+                SupportsSuper = true,
+                XP11 = true,
+            };
+
+            var r1129 = new Runway
+            {
+                HasLighting = true,
+                Length = 11487
+            };
+
+            r1129.RunwayEnds.Add(new RunwayEnd
+            {
+                Name = "11",
+                HasClosedMarkings = false,
+                Latitude = 48.12283706665039,
+                Longitude = 16.53326416015625
+            });
+
+            r1129.RunwayEnds.Add(new RunwayEnd
+            {
+                Name = "29",
+                HasClosedMarkings = false,
+                Latitude = 48.109031677246094,
+                Longitude = 16.57568359375
+            });
+
+            var r1634 = new Runway
+            {
+                HasLighting = true,
+                Length = 11819
+            };
+
+            r1634.RunwayEnds.Add(new RunwayEnd
+            {
+                Name = "16",
+                HasClosedMarkings = false,
+                Latitude = 48.119815826416016,
+                Longitude = 16.578155517578125
+            });
+
+            r1634.RunwayEnds.Add(new RunwayEnd
+            {
+                Name = "34",
+                HasClosedMarkings = false,
+                Latitude = 48.088619232177734,
+                Longitude = 16.591339111328125
+            });
+
+            loww.Runways.Add(r1129);
+            loww.Runways.Add(r1634);
+
+            package.Airports.Add(loww);
+
+            Debug.WriteLine("LOWW package:");
+            json = package.CreatePackage(false);
+            Debug.WriteLine(json);
+            Assert.AreNotEqual(emptyHash, package.Hash);
+
+            parsedPackage = AirportPackage.FromPackageJson(json, false);
+            Assert.AreEqual(parsedPackage.Hash, package.Hash);
+            Assert.AreEqual(parsedPackage.CreatePackage(false), package.CreatePackage(false));
+            Assert.AreEqual(parsedPackage.CreatePackage(), package.CreatePackage());
+
+            Debug.WriteLine("Compression tests:");
+            json = package.CreatePackage();
+            Debug.WriteLine(json);
+
+            Assert.ThrowsException<JsonReaderException>(() => AirportPackage.FromPackageJson(json, false));
+            parsedPackage = AirportPackage.FromPackageJson(json);
+            Assert.AreEqual(parsedPackage.Hash, package.Hash);
+            Debug.WriteLine(parsedPackage.CreatePackage(false));
         }
     }
 }
---
+++ OpenSky.AirportsJSON/AirportPackage.cs
@@ -0,0 +1,111 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AirportPackage.cs" company="OpenSky">
+// OpenSky project 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.AirportsJSON
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// The airports package.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 03/12/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public class AirportPackage
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the airports.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public List<Airport> Airports { get; set; } = new List<Airport>();
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the SHA-256 hash value of the package (added as a last step before saving).
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public string Hash { get; set; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Creates package object from (compressed) JSON string.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/12/2021.
+        /// </remarks>
+        /// <exception cref="Exception">
+        /// If JSON cannot be parsed.
+        /// </exception>
+        /// <param name="jsonString">
+        /// The JSON string.
+        /// </param>
+        /// <param name="isCompressed">
+        /// (Optional) True if the JSON string is gzipped.
+        /// </param>
+        /// <returns>
+        /// The airports package.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        public static AirportPackage FromPackageJson(string jsonString, bool isCompressed = true)
+        {
+            if (isCompressed)
+            {
+                jsonString = jsonString.DecompressFromBase64();
+            }
+
+            var package = JsonConvert.DeserializeObject<AirportPackage>(jsonString);
+            if (package == null)
+            {
+                throw new Exception("Error deserializing airport client package JSON.");
+            }
+
+            return package;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Creates a gzip compressed, base64 encoded json string and updates the Hash property.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/12/2021.
+        /// </remarks>
+        /// <param name="compress">
+        /// (Optional) True to compress the output using gzip.
+        /// </param>
+        /// <returns>
+        /// The new package.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        public string CreatePackage(bool compress = true)
+        {
+            // Make sure not to include the hash property for the new SHA256 calculation
+            this.Hash = null;
+
+            var jObject = JObject.FromObject(this);
+            var jsonString = jObject.ToString(Formatting.None);
+
+            using (var sha256Hash = SHA256.Create())
+            {
+                var jsonHash = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(jsonString));
+                this.Hash = Convert.ToBase64String(jsonHash);
+            }
+
+            jObject = JObject.FromObject(this);
+            jsonString = jObject.ToString(Formatting.None);
+
+            return compress ? jsonString.CompressToBase64() : jsonString;
+        }
+    }
+}
